3c96a92bde65825c57de00817689e4f396fabbf2
n0rppa <82247655+n0rppa@users.noreply.github.com>
2025-06-26 07:33:09 +0200

    Update visualizer.js

--- music-player/js/visualizer.js
+++ music-player/js/visualizer.js
@@ -1,4 +1,4 @@
-function setupVisualizer(analyser, canvas) {
+export default function setupVisualizer(analyser, canvas) {
     const ctx = canvas.getContext('2d');
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
@@ -8,13 +8,16 @@ function setupVisualizer(analyser, canvas) {
         analyser.getByteFrequencyData(dataArray);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        dataArray.forEach((value, index) => {
-            ctx.fillStyle = `rgb(${value}, ${value / 2}, ${255 - value})`;
-            ctx.fillRect(index * 5, canvas.height - value, 4, value);
-        });
-    }
+        const barWidth = (canvas.width / bufferLength) * 2.5;
+        let barHeight;
+        let x = 0;
 
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] / 1.5;
+            ctx.fillStyle = `rgb(${barHeight + 100},50,${255 - barHeight})`;
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth + 1;
+        }
+    }
     draw();
 }
-
-export default setupVisualizer;
